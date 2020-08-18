a06d41768844878ff7710358e030a7a12fc83838
Mustafadagkiranlar <dagkiranlar.m@gmail.com>
2020-08-18 13:48:51 +0300

    updated

--- gui/test.py
+++ gui/test.py
@@ -2,8 +2,12 @@ from tkinter import *
 from tkinter import ttk
 
 def calculate(*args):
-    meter = feet * 0.304785126
-    return(meter)
+    try:
+        value = float(feet.get())
+        meters.set((0.3048 * value * 10000.0 + 0.5)/10000.0)
+    except ValueError:
+        meters.set("Please enter number")
+        pass
 
 root = Tk()
 root.title("Feet to Meters")
@@ -22,12 +26,11 @@ feet_entry.grid(column=2, row=1, sticky=(W, E))
 ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
 ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
 
-
 ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
 ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
 ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
 
-for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)#adds padding all of the widgets
 
 feet_entry.focus()
 root.bind('<Return>', calculate)
